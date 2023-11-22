ea100bb5921fcb2a12ab9283653516bc8d7e93dc
HazelFannn <jiayu.fan@mail.utoronto.ca>
2023-11-21 23:54:00 -0500

    update gitignore

--- Docker/noetic.bash
+++ Docker/noetic.bash
@@ -1,15 +1,10 @@
-xhost local:root
-XAUTH=/tmp/.docker.xauth
 docker run --rm -it \
-    --name=grasp_control_container\
-    --env="DISPLAY=$DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --env="XAUTHORITY=$XAUTH" \
-    --volume="/home/$USER/catkin_ws/src/grasp_icp:/catkin_ws/src/grasp_icp" \
+    --name=drl_container\
+    -e DISPLAY=host.docker.internal:0.0
+    --volume="C:\Users\hazel\Documents\MEng Project\TD3_Navigation:/catkin_ws/src/TD3_Navigation" \
     --volume="/dev/bus/usb:/dev/bus/usb" \
     --net=host \
     --privileged \
     ros-noetic \
     bash
-
-echo "Done."
+docker run --rm --name drl_container -e DISPLAY=host.docker.internal:0.0 -v -it ros-noetic
