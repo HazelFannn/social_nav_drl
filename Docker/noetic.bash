bbfe1d2bed6a5ac11dc92437146e77fc1227279c
HazelFannn <jiayu.fan@mail.utoronto.ca>
2023-12-16 19:42:45 -0500

    updated files

--- Docker/noetic.bash
+++ Docker/noetic.bash
@@ -9,3 +9,13 @@ docker run --rm -it \
     bash
 docker run --rm -t -d --name drl_container -e DISPLAY=host.docker.internal:0 -v "C:\Users\hazel\Documents\MEng Project\TD3_Navigation\:/home" -it ros-noetic-nvidia
 docker exec -it <id> bash
+
+cd home/catkin
+catkin_make_isolated
+export GAZEBO_RESOURCE_PATH=/home/catkin_ws/src/multi_robot_scenario/launch
+source devel_isolated/setup.bash
+python3 train_velodyne_td3.py
+
+killall -9 rosout roslaunch rosmaster gzserver nodelet robot_state_publisher gzclient python python3
+
+docker run --rm --gpus all --privileged -v /dev:/dev nvidia/cuda:12.3-base nvidia-smi
