97dc63563852453f612613c54d5dc3a0d8663849
JensenzhaoUT <Jensen.zhao@mail.utoronto.ca>
2024-02-01 23:47:14 -0500

    tested simple pedestrian for melodic

--- docker_melodic/drl.bash
+++ docker_melodic/drl.bash
@@ -5,6 +5,7 @@ docker run --rm -it \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --env="XAUTHORITY=$XAUTH" \
+    --env="XDG_RUNTIME_DIR=/tmp/runtime-root"  \
     --volume="/home/hazel/catkin_ws/src/social_nav_drl:/catkin_ws/src/social_nav_drl" \
     --volume="/dev/bus/usb:/dev/bus/usb" \
     --gpus all \
