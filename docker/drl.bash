0486b92312e7260054f664d793587b4eab4b6c0d
HazelFannn <hazel.fan@mail.utoronto.ca>
2024-02-08 22:26:02 -0500

    add volume

--- docker/drl.bash
+++ docker/drl.bash
@@ -5,6 +5,7 @@ docker run --rm -it \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --env="XAUTHORITY=$XAUTH" \
+    --volume="/home/hazel/catkin_ws/src/social_nav_drl:/catkin_ws/src/social_nav_drl/" \
     --volume="/dev/bus/usb:/dev/bus/usb" \
     --gpus all \
     --net=host \
@@ -14,4 +15,3 @@ docker run --rm -it \
     bash
 
 echo "Done."
-# --volume="/home/hazel/catkin_ws/src/social_nav_drl:/catkin_ws/src/social_nav_drl" \
